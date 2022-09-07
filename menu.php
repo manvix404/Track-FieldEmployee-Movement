a10e5c97b4ee3a422ad3c898f1a6af4f0f30f906
k-arthik-raj <91473761+k-arthik-raj@users.noreply.github.com>
2022-09-07 21:53:22 +0530

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Financial advice</title>
+    <link rel="stylesheet" href="/J-component/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+<!---<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">--->
+</head>
+<body>
+   <div class="container">
+       <div class="navbar">
+        <div class="logo">
+            <a href="index.php"><img src="images/logo.png" width="125px"></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="products.php">Products</a></li>
+                <!---<li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>---->
+                <li><a href="account.php">Account</a></li>
+                <li><a href="seller.php">Sell</a></li>
+            </ul>
+        </nav>
+        <a href="cart.php"><img src="images/cart_1.png" width="30px height 30px"></a>
+    </div>    
+   </div>
